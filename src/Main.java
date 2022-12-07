3669a559a900579cfefa9d2a116075bd9799ab27
Edwin Ryerson <eryerson06@gmail.com>
2022-12-07 11:27:03 -0700

    Create First JFrame

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame jf = new JFrame();
-        jf.setTitle("First JFrame");
+        jf.setTitle("Demo Game");
         jf.setBounds(100, 100, 400, 300);
         jf.setVisible(true);
     }
